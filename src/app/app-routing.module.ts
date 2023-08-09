9021fdcf029e3b650c86669e46ef16092eb684fd
vic <yzhang3674@gatech.edu>
2023-08-09 00:26:44 -0400

    Add blog detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { NewBlogComponent } from './new-blog/new-blog.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'admin', component: AdminComponent }
+  { path: 'admin', component: AdminComponent },
+  { path: 'create', component: NewBlogComponent },
+  { path: 'blog', component: BlogDetailComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeComponent } from './home/home.component';
 import { BlogCardComponent } from './blog-card/blog-card.component';
 import { FooterComponent } from './footer/footer.component';
 import { AdminComponent } from './admin/admin.component';
+import { NewBlogComponent } from './new-blog/new-blog.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
+import { FormsModule } from '@angular/forms';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,17 @@ import { AdminComponent } from './admin/admin.component';
     HomeComponent,
     BlogCardComponent,
     FooterComponent,
-    AdminComponent
+    AdminComponent,
+    NewBlogComponent,
+    BlogDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
+    FormsModule,
+    LMarkdownEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
